31b86a4472a88b4aa9a7d0763572008b051b0e55
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-08-28 05:44:27 +0600

    Add files via upload

---
+++ uri 1003 simple sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, SOMA;
+
+    A = -30;
+    B = 10;
+
+    SOMA = A + B;
+
+    cout << "SOMA = " << SOMA << endl;
+
+    return 0;
+}
