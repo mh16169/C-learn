792413776a41f500cd10de3373104bc5b03587a6
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-08-27 07:51:42 +0600

    Add files via upload

---
+++ personal info print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << endl;
+    cout << "Name: Md Mahedi Hasan \n";
+    cout << "ID: 231-0220-201" << endl;
+    cout << "Sec: Evenig" << endl
+         << "Dept: CSE" << endl
+         << "Universty: ADUST" << endl
+         << endl;
+
+    return 0;
+}
