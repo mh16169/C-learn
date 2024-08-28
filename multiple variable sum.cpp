28e5de1128cb21e1eba4b903ca140b911b477204
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-08-29 04:58:55 +0600

    Add files via upload

---
+++ multiple variable sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int var1 = 21, var2 = 32, var3 = 405, var4 = 5;
+
+    cout << "your result is : " << var1 + var2 + var3 + var4 << endl;
+
+    return 0;
+}
