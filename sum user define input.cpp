b70b9149c422a81203b027aaf6f2e1b658f51e2f
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-08-30 23:54:49 +0600

    Add files via upload

---
+++ sum user define input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a, b, result;
+    cout << "Enter Tow Number: ";
+
+    cin >> a >> b;
+    result = a + b;
+    cout << "your result is a+b = " << result << endl;
+
+    return 0;
+}
