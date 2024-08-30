7af65882887b9d39c713c475d12fc2be5da03e68
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-08-31 03:43:19 +0600

    Add files via upload

---
+++ bool true false.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    bool present, absent;
+
+    present = true;
+
+    absent = false;
+
+    cout << "Student is present : " << present << endl;
+
+    cout << "Student is Absent : " << absent << endl;
+
+    return 0;
+}
