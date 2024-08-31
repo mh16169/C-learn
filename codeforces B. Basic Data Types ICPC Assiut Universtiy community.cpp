3de4ec21a0a0274d415479b9074e49e4c4d2fe0e
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-08-31 14:25:14 +0600

    Add files via upload

---
+++ codeforces B. Basic Data Types ICPC Assiut Universtiy community.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int number;
+    long long int longnumber;
+    char charcater;
+    float fnumber;
+    double dnumber;
+
+    cin >> number >> longnumber >> charcater >> fnumber >> dnumber;
+
+    cout << number << endl;
+    cout << longnumber << endl;
+    cout << charcater << endl;
+    cout << fixed << setprecision(2) << fnumber << endl;
+    cout << fixed << setprecision(1) << dnumber << endl;
+
+    return 0;
+}
