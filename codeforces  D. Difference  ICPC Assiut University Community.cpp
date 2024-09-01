d29ebe44b338bf4c6ac2e9e510233b1599dde8ac
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-09-01 06:57:11 +0600

    Add files via upload

---
+++ codeforces  D. Difference  ICPC Assiut University Community.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long A, B, C, D, X;
+
+    cin >> A >> B >> C >> D;
+
+    X = (A * B) - (C * D);
+
+    cout << "Difference = " << X << endl;
+
+    return 0;
+}
