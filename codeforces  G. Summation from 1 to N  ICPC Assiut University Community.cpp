65c15350ceb4101fa69c116aad79606d06eef575
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-09-02 01:16:23 +0600

    Add files via upload

---
+++ codeforces  G. Summation from 1 to N  ICPC Assiut University Community.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long int n, sum;
+
+    cin >> n;
+
+    sum = (n * (1 + n)) / 2;
+
+    cout << sum << endl;
+    return 0;
+}
