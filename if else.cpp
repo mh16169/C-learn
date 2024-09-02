aa14a89aac3ca6163e380676bb0e033234058d3e
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-09-03 02:22:48 +0600

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int id;
+    cout << "Enter your Id : ";
+    cin >> id;
+
+    if (id == 16169)
+    {
+        cout << "Id match, Welcome Md Mehedi Hasan" << endl;
+    }
+    else
+    {
+        cout << "Id dont't match, Sorry your not the correct person";
+    }
+
+    return 0;
+}
