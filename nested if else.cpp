62feed9705d78f102fe5e8b754c0fd18e65cd5e4
Md Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2024-09-04 14:31:48 +0600

    Add files via upload

---
+++ nested if else.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int id;
+    cout << "Enter your Id : ";
+    cin >> id;
+
+    if (id == 16169)
+    {
+        cout << "Id match, Welcome Md Mehedi Hasan" << endl;
+
+        int exp;
+
+        exp = 3;
+        if (exp >= 3)
+        {
+            cout << "You are the Senior Staff, we proud of you, Your exp is " << exp << " years" << endl;
+        }
+    }
+    else
+    {
+        cout << "Id dont't match, Sorry your not the correct person";
+    }
+
+    return 0;
+}
